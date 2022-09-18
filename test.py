3cf352b7562d6015b1a49d38c707045ce4b09836
Aingaran Elango <eaingaran@gmail.com>
2022-09-18 11:22:01 +0200

    committing local test file

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
 import json
 import time
 import traceback
+import math
+import timeit
+import os
 
 import requests
 
@@ -124,10 +127,136 @@ def create_a330_routes():
 def do_maintanance():
     am4.do_maintanance()
 
+def do_marketing():
+    am4.login(am4.username, am4.password)
+    am4.marketing()
+    am4.logout()
+
+
+def get_all_routes():
+    p_routes = []
+    am4.login(am4.username, am4.password)
+    planes = []
+    hubs = []
+    with open('planes.json', 'r') as planes_json:
+        planes = json.load(planes_json)['pax']
+    with open('hubs.json', 'r') as hubs_json:
+        hubs = json.load(hubs_json)
+    plane = [plane for plane in planes if plane['shortname'] == 'a388'][0]
+    balance = am4.get_balance()
+    # if balance > plane['price'] * 1.3:
+    for hub in hubs:
+        print(f'processing hub {hub["iata"]}')
+        routes = am4.find_pax_routes(plane, hub['iata'], 100)
+        if routes is None or len(routes) == 0:
+            continue
+        for name, route in routes.items():
+            print(f'can buy {name}')
+            print(f"{plane['id']}, {hub['hub_id']}, {plane['engine']['id']}, {name}, {route['economy']}, {route['business']}, {route['first']}")
+            p_routes.append(f"{plane['id']}, {hub['hub_id']}, {plane['engine']['id']}, {name}, {route['economy']}, {route['business']}, {route['first']}, {route['distance']}, {route['trips']} \n")
+    am4.logout()
+    with open('routes_to_buy.txt', 'w+') as routes_file:
+        routes_file.writelines(p_routes)
+
+
+def get_all_cargo_routes():
+    p_routes = []
+    am4.login(am4.username, am4.password)
+    planes = []
+    hubs = []
+    with open('planes.json', 'r') as planes_json:
+        planes = json.load(planes_json)['cargo']
+    with open('hubs.json', 'r') as hubs_json:
+        hubs = json.load(hubs_json)
+    plane = [plane for plane in planes if plane['shortname'] == 'a388f'][0]
+    balance = am4.get_balance()
+    # if balance > plane['price'] * 1.3:
+    for hub in hubs:
+        print(f'processing hub {hub["iata"]}')
+        routes = am4.find_cargo_routes(plane, hub['iata'], 100)
+        if routes is None or len(routes) == 0:
+            continue
+        for name, route in routes.items():
+            print(f'can buy {name}')
+            print(f"{plane['id']}, {hub['hub_id']}, {plane['engine']['id']}, {name}, {route['aft']}, {route['fwd']}")
+            p_routes.append(f"{plane['id']}, {hub['hub_id']}, {plane['engine']['id']}, {name}, {route['aft']}, {route['fwd']}, {route['distance']}, {route['trips']} \n")
+    am4.logout()
+    with open('routes_to_buy.txt', 'w+') as routes_file:
+        routes_file.writelines(p_routes)
+
+
+def check_available_380_routes():
+    planes = []
+    hubs = []
+    with open('planes.json', 'r') as planes_json:
+        planes = json.load(planes_json)['pax']
+    with open('hubs.json', 'r') as hubs_json:
+        hubs = json.load(hubs_json)
+    plane = [plane for plane in planes if plane['shortname'] == 'a388'][0]
+    plane_details = am4.get_pax_plane_details(2)
+
+    # include a339 routes in check as well...
+    plane_details.extend(am4.get_pax_plane_details(308))
+    all_routes = []
+
+    for hub in hubs:
+        routes = am4.find_pax_routes(plane, hub['iata'], plane_details, 1000)
+        all_routes.extend(routes)
+    
+    print(f'found {len(all_routes)} routes for A380-800')
+
+
+def check_plane_profits(aircraft_type_id):
+    planes_data = []
+
+    driver = am4.get_driver()
+
+    driver.get(
+        f'https://www.airlinemanager.com/fleet.php?type={aircraft_type_id}')
+
+    elements = driver.find_elements(By.XPATH, '/html/body/div[2]/div/div')
+
+    for element in elements:
+        try:
+            plane_id = element.find_element(
+                By.XPATH, f'.//div[1]/span').get_attribute("onclick").split(',')[1]
+            plane_name = element.find_element(
+                By.XPATH, f'.//div[2]/a').text
+            plane_link = f'https://www.airlinemanager.com/fleet_details.php?id={plane_id}&returnType={aircraft_type_id}'
+
+        except Exception as e:
+            print(e)
+
+        planes_data.append({'id': plane_id, 'name': plane_name, 'url': plane_link})
+
+    for plane in planes_data:
+        driver.get(plane['url'])
+        age = driver.find_element(By.XPATH, f'//*[@id="detailsGroundedBg"]/div[4]/div/div[1]/span[4]').text
+        count = 0
+        total = 0
+        for i in range(1, 21):
+            try:
+                revenue = int(driver.find_element(By.XPATH, f'//*[@id="flight-history"]/div[{i}]/div[4]').text.replace('$', '').replace(',', ''))
+                total += revenue
+                count += 1
+            except Exception as e:
+                traceback.print_exc()
+                break
+        if count != 0:
+            average_revenue = total / count
+        else:
+            average_revenue = 0
+        plane['avg_revenue'] = average_revenue
+        plane['age'] = age
+    
+    sorted_plane_data = sorted(planes_data, key=lambda d: d['avg_revenue']) 
+
+    for plane in sorted_plane_data:
+        print(f'{plane["name"]} bought {plane["age"]} has an average revenue of {plane["avg_revenue"]}')
 
 if __name__ == '__main__':
-    am4.username = ''
-    am4.password = ''
+    am4.username = os.getenv("AM_USERNAME", "")
+    am4.password = os.getenv("AM_PASS", "")
     am4.fuel_price_threshold = '100'
     am4.co2_price_threshold = '100'
 
@@ -136,13 +265,39 @@ if __name__ == '__main__':
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
     w_driver = webdriver.Chrome(options=options,
-                                executable_path='')
+                                executable_path='~/Sources/PycharmProjects/am4/drivers/chromedriver')
     w_driver.maximize_window()
 
     am4.w_driver = w_driver
 
+    starttime = timeit.default_timer()
+    print("The start time is :",starttime)
+    # get_all_routes()
     # am4.run_app()
+    # get_all_cargo_routes()
+    # get_all_routes()
+    
+    am4.login(am4.username, am4.password)
+    # planes_data = am4.get_cargo_plane_details(358)
+    #print(planes_data.__repr__())
+    # am4.buy_pax_aircrafts()
 
+    #check_available_380_routes()
+    check_plane_profits(308)
+    
+    am4.logout()
+
+    print("The time difference is :", timeit.default_timer() - starttime)
+
+    #get_all_routes()
+
+    # hangars.php?mode=upgrade&amount=10&type=
+    # hangars.php?mode=upgrade&amount=10&type=cargo
+
+    # 'https://www.airlinemanager.com/ac_order_do_cargo.php?engine=336&reg=TEST&hub=3353515&acId=365&aft=20&fwd=10'
+
+    # am4.run_app()
+    # do_marketing()
     # update_ticket_price()
     # buy_planes(Planes.A330_900neo.value, Hubs.Los_Angeles.value, Engines.RR_Trent_7000.value)
     # print_routes()
